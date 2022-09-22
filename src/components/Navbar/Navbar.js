82a58f5fe3577adc1fa62631006b96f4e3218077
abrar997 <abraralrawi997.com>
2022-09-22 20:32:20 +0300

    check navbar for small screen

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,33 +2,122 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { icons } from "../../constants";
 import { motion } from "framer-motion";
+
+const navbarsmallscreen = {
+  open: {
+    opacity: 1,
+    y: 90,
+  },
+  close: { opacity: 0, y: 0,
+  transition:{
+    duration:1,
+  ease: "easeInOut"
+  }
+  },
+};
+
 const Navbar = () => {
-const [Active,setActive] =React.useState("home")
+  const [Active, setActive] = React.useState("home");
+  const [show, setShow] = React.useState(false);
   return (
-    <div className=" flex justify-between">
+    <div className=" flex justify-between ">
       <div className="flex gap-10">
         <div className=" text-logo"> TechBoom </div>
-        <div>
+        <div className="invisible xl:visible lg:visible md:visible">
           <ul className="flex  gap-7 text-[1.2rem] capitalize mt-3 ">
-            <li onClick={()=>setActive("work")} className={Active==="work" ? " border-b-2 border-black" :""}><Link to=""> work </Link>
-             </li>
-            <li onClick={()=>setActive("about")} className={Active==="about" ? " border-b-2 border-black" :""}><Link to=""> about</Link> </li>
-            <li onClick={()=>setActive("contract")} className={Active==="contract" ? " border-b-2 border-black" :""}><Link to=""> contract </Link></li>
+            <li
+              onClick={() => setActive("work")}
+              className={Active === "work" ? " border-b-2 border-black" : ""}
+            >
+              <Link to=""> work </Link>
+            </li>
+            <li
+              onClick={() => setActive("about")}
+              className={Active === "about" ? " border-b-2 border-black" : ""}
+            >
+              <Link to=""> about</Link>
+            </li>
+            <li
+              onClick={() => setActive("contract")}
+              className={
+                Active === "contract" ? " border-b-2 border-black" : ""
+              }
+            >
+              <Link to=""> contract </Link>
+            </li>
           </ul>
         </div>
       </div>
-      <div> 
-          <ul className=" flex gap-7 text-[1.2rem] capitalize mt-3">
-          <motion.li whileHover={{color:"brown",scale:"1.1"}}
-           transition={{duration:"0.8s",ease:"easeInOut"}}
-          ><a href=""><icons.BsInstagram/> </a> </motion.li>
-          
-           <motion.li whileHover={{color:"brown",scale:"1.1"}}
-           transition={{duration:"0.8s",ease:"easeInOut"}}>
-           <a href=""><icons.BsTwitter/> </a> 
-           </motion.li>
-          </ul>
-         </div>
+      <div>
+        <ul className=" flex gap-7 text-[1.2rem] capitalize mt-3 invisible xl:visible lg:visible md:visible">
+          <motion.li
+            whileHover={{ color: "brown", scale: "1.1" }}
+            transition={{ duration: "0.8s", ease: "easeInOut" }}
+          >
+            <a href="">
+              <icons.BsInstagram />
+            </a>
+          </motion.li>
+
+          <motion.li
+            whileHover={{ color: "brown", scale: "1.1" }}
+            transition={{ duration: "0.8s", ease: "easeInOut" }}
+          >
+            <a href="">
+              <icons.BsTwitter />
+            </a>
+          </motion.li>
+        </ul>
+      </div>
+      {/* small  screen */}
+      <motion.div  className="visible xl:invisible lg:invisible md:invisible">
+        <motion.div
+          initial={{ scale: 1, rotate: 0, opacity: 0.9 }}
+          whileHover={{
+            scale: 1.1,
+            opacity: 1,
+            transition: {
+              duration: 0.8,
+              ease: "easeInOut",
+              type: "spring",
+            },
+          }}
+          className="flex justify-center   items-center text-logo cursor-pointer"
+          onClick={() => setShow(!show)}
+        >
+          {show ? <icons.AiOutlineClose /> : <icons.HiMenuAlt4 />}
+        </motion.div>
+
+          <motion.ul
+          initial={{y:0,opacity:0}}
+          variants={navbarsmallscreen}
+          animate={show? "open" : "close"}
+          className=" fixed  w-[100%] h-[100%] left-0 right-0 justify-center flex flex-col items-center 
+          text-logo bg-white z-10"
+          >
+            <li className="border-b-2 border-[brown] hover:text-[grey]">
+              <Link to="">Work</Link>
+            </li>
+            <li className="border-b-2 border-[brown] hover:text-[grey]">
+              <Link to="">About</Link>
+            </li>
+            <li className="border-b-2 border-[brown] hover:text-[grey]">
+              <Link to="">Contract</Link>
+            </li>
+            <div className="flex gap-6 mt-10 mr-auto items-center ">
+              <li className="text-[grey] hover:text-[brown]">
+                <a href="">
+                  <icons.BsInstagram />
+                </a>
+              </li>
+              <li className="text-[grey] hover:text-[brown]">
+                <a href="">
+                  <icons.BsTwitter />
+                </a>
+              </li>
+            </div>
+          </motion.ul>
+          </motion.div>
     </div>
   );
 };
--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,6 +1,11 @@
 import {BsHeart, BsTwitter, BsInstagram } from "react-icons/bs";
+import {HiMenuAlt4} from "react-icons/hi";
+import {AiOutlineClose} from "react-icons/ai";
+
 export default {
   BsInstagram,
   BsTwitter,
-  BsHeart
+  BsHeart,
+  HiMenuAlt4,
+  AiOutlineClose
 };
