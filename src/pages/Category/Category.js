3ff3f61d8dec714595e93272280d68cfea886234
abrar997 <abraralrawi997.com>
2022-09-23 12:52:45 +0300

    done

--- src/pages/Category/Category.js
+++ src/pages/Category/Category.js
@@ -1,24 +1,37 @@
 import React from "react";
 import { data, icons } from "../../constants";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 const Category = () => {
   return (
-    <div className="mt-[50px]">
+    <motion.div
+    initial={{opacity:0}}
+    animate={{opacity:1,transition:{
+      duration:1
+    }}}
+    className="mt-[50px]">
       <h1 className=" capitalize xl:text-[3rem] lg:text-[3rem] text-[1.8rem] ">
         ceramics Products
       </h1>
-      <p className="xl:w-[50%] lg:w-[50%] xl:text-[21px] lg:text-[21px] text-[16px] md:text-[18px]">
+      <motion.p 
+      initial={{opacity:0,x:-30}}
+      animate={{x:0,opacity:1,transition:{duration:0.7}}}
+      className="xl:w-[50%] lg:w-[50%] xl:text-[21px] lg:text-[21px] text-[16px] md:text-[18px]">
         Quam. Sunt inventore nesciunt, vitae aliquip. Nostrud veniam doloremque
         consectetur so consectetur but laboriosam. Esse anim sed but aut or
         voluptas yet voluptate. Laudantium ex so autem consequuntur. Aliquid.
         Velitesse pariatur or non so dolores, or inventore and laboris. Ullamco.
         Ipsa molestiae yet adipisicing, yet commodi.
-      </p>
+      </motion.p>
       <div className="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 xl:grid-cols-2 xl:gap-10 lg:gap-10 gap-3 mt-[50px]">
         {data.ceramics.map((item, index) => {
           return (
             <div className="" key={index}>
-              <img
+              <motion.img
+              initial={{scale:0.9,opacity:0}}
+              whileInView={{scale:1,opacity:1,transition:{
+                duration:0.7
+              }}}
                 src={item.img}
                 className="xl:h-[900px] lg:h-[800px] h-[500px] 
                   w-[100%] object-cover"
@@ -27,14 +40,17 @@ const Category = () => {
           );
         })}
       </div>
-      <p className="xl:w-[51%] lg:w-[50%] md:w-[55%]  mt-[50px] xl:text-[21px] lg:text-[21px] text-[16px] md:text-[18px]">
+      <motion.p 
+        initial={{opacity:0,x:-30}}
+        whileInView={{x:0,opacity:1,transition:{duration:0.7}}}
+        className="xl:w-[51%] lg:w-[50%] md:w-[55%]  mt-[50px] xl:text-[21px] lg:text-[21px] text-[16px] md:text-[18px]">
         Cat ipsum dolor sit amet, fugiat iste. Totam aliqua, yet ab, for natus
         for suscipit. Minim eos molestiae. Enim irure officia and minima.
         Adipisicing ratione. Velitesse elit for magni aliquam. Qui corporis
         tempora quam, so est, magni. Nemo. Irure. Eos illum labore. Accusantium
         eu. Odit sit and cillum sed eaque, but quia, duis. Aliqua minima do
         adipisicing, doloremque.
-      </p>
+      </motion.p>
       <p className="mt-10 capitalize text-[19px] font-semibold">
         <i> ceramics by abrar & saliha </i>
       </p>
@@ -45,7 +61,7 @@ const Category = () => {
         <Link to="">Watches types</Link>
         <icons.AiOutlineArrowRight />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
